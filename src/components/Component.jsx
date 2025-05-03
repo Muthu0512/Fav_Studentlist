cc3138fc6355fbd75f459060d839fe5b3019c837
Muthu0512 <masmuthu99@gmail.com>
2025-05-03 22:09:35 +0530

    small updates are done

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -17,7 +17,7 @@ const ContextProvider = ({ children }) => {
       <section className="bg-orange-400 flex justify-start gap-40 px-10 py-5 text-3xl ">
 
         <button className="bg-violet-600 p-3 border rounded-lg border-none hover:bg-violet-800  hover:underline" onClick={() => navigate("/") } >StudentList</button>
-        <button className="bg-emerald-600 p-3 border rounded-lg border-none hover:bg-emerald-800 hover:underline" onClick={() => navigate("/favlist")} >Favorite Students</button>
+        <button className="bg-emerald-600 p-3 border rounded-lg border-none hover:bg-emerald-800 hover:underline" onClick={() => navigate("/favlist")} >Favourite Students</button>
         
       </section>
       <section>{children}</section>
